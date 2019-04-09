c5e5eddac1a6472a4a95dbf1eb1c075681621bce
mgoldsm2 <47144163+mgoldsm2@users.noreply.github.com>
2019-04-09 15:45:11 -0400

    Script
    Second Step Loops was made

--- startercode.m
+++ startercode.m
@@ -4,7 +4,7 @@ filename = 'latlon.xlsx';
 geos215 = readtable(filename);
 geos215 = table2array(geos215);
 geos215S1 = xlsread(filename,'Sheet2');
-%geos215S1 = table2array(geos215S1);
+
 
 figure(1);clf;
     worldmap('USA'); geoshow('landareas.shp','FaceColor','black')
@@ -18,54 +18,15 @@ figure(1);clf;
  %Paleozoic
 filename = 'Paleozoic_strata_1994-2012_filt.xlsx'; 
 strata = xlsread(filename,'Gas_shale_db');
-%strata = readtable(filename);
-%strata = table2array(strata);
 
 %%
 %Rock Geo
 filename = 'tblRockGeoData_filt.xlsx';
-%rockGeo = xlsread(filename);
 rockGeo = readtable(filename);
-%rockGeo = table2array(rockGeo);
+
 
 
 %%
 %Chem
 filename = 'xtbXrfChem_filt.xlsx';
-%chem = xlsread(filename);
 chem = readtable(filename);
-%chem = table2array(chem);
-
- 
-%% 
- %use something similar to the CO2 data lab to match the LAB_ID values from
- %the table and xtb files so that xtb file has corresponding lat and lon so we can graph it
- 
- %longrid = unique(geos215.LONGITUTE);
- %latgird = unique(geos215.LATITUDE);
- 
- %% use something similar to the CO2 data lab to match the LAB_ID values from
- %the table and xtb files so that xtb file has corresponding lat and lon so we can graph it
-for i=1:38581
-    indName =find(strcmp(rockGeo.https___mrdata_usgs_gov_ngdb_ngdbrock_tab_zip, chem.LAB_ID(i)));
-    chem.LAT(i) = rockGeo.Var5(indName);
-    chem.LON(i) = rockGeo.Var6(indName);
-end
-
-%% 2b. Pull out the seawater pCO2 (PCO2_SW) and sea surface temperature (SST)
-%data and reshape it into your new 3-dimensional arrays
-%  for i = 1:21084
-%      indLON = find(CO2data.LON(i) == longrid);
-%      indLAT = find(CO2data.LAT(i) == latgrid);
-%      indM = find(CO2data.MONTH(i) == monthgrid);
-%      CO2_SWgrid(indLON, indLAT, indM) = CO2data.PCO2_SW(i);
-%      SSTgrid(indLON, indLAT, indM) = CO2data.SST(i);
-%  end
-%%%% 
- %use something similar to the CO2 data lab to match the LAB_ID values from
- %the table and xtb files so that xtb file has corresponding lat and lon so we can graph it
- 
- %longrid = unique(geos215.LONGITUTE);
- %latgird = unique(geos215.LATITUDE);
- 
-%%
