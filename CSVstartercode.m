9d72437240d08e1a97ba93c700823ff0417c3044
exjackman <40804895+exjackman@users.noreply.github.com>
2019-04-10 15:41:24 -0400

    starting graphs CSV

--- CSVstartercode.m
+++ CSVstartercode.m
@@ -1,22 +1,14 @@
-% Loading csv and merging into a singular matlab file
-% tabularTextDatastore(filepath - filefolder)
-% “”.VariableNames =  {single quote names}
-% “”.TextscanFormats = {the signs and types and formats}
-% Order of the column names need to be the same order
-% Can use excel to add columns as dummy columns 
-% readall(“”) into a variable name
-% Can right click a variable in the workspace and save as a .mat file
-% To find indexes they need to matrices
-% cell2mat(table2cell(the cells variable))
+%%  Starter script for No-People-No-Climate
+%Paleozoic
+    filename = 'Paleozoic_strata_1994-2012_filt.csv'; 
+    strata = readtable(filename);
+%Rock Geo
+    filename = 'tblRockGeoData_filt.csv';
+    rockGeo = readtable(filename);
 
-ttds = tabularTextDatastore('C:\Users\emmal\Documents\GitHub\team-research-project-no-people-no-climate',...
-    'FileExtensions','.csv');
-ttds.TextscanFormats = {'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',   '%s',	'%s',...
-                         '%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',   '%s',	'%s',	'%s',...
-                         '%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',   '%s',	'%s',	'%s',...
-                         '%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',   '%s',	'%s',	'%s',...
-                         '%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',	'%s',   '%s',	'%s',	'%s',...
-                         '%s',	'%s',	'%s',   '%s',	'%s'};
-bigdata = readall(ttds);
-%rockmatrix = cell2mat(table2cell(bigdata));
+
 %%
+figure(1);clf;
+    usamap({'ME','MS'}); geoshow('landareas.shp','FaceColor','black')
+    scatterm(strata.LATITUDE, strata.LONGITUDE, strata.Quartz_, 'filled'); colorbar;
+    %scatterm(geos215S1(:,1), geos215S1(:,2), 'r.');
