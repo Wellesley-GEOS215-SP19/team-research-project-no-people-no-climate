de123f87c0918ff6de28496c7eec59505e7ab83f
mgoldsm2 <47144163+mgoldsm2@users.noreply.github.com>
2019-04-12 12:46:58 -0400

    Era sorting
    Era sorting

--- CSVstartercode.m
+++ CSVstartercode.m
@@ -12,3 +12,24 @@ figure(1);clf;
     usamap({'ME','MS'}); geoshow('landareas.shp','FaceColor','black')
     scatterm(strata.LATITUDE, strata.LONGITUDE, strata.Quartz_, 'filled'); colorbar;
     %scatterm(geos215S1(:,1), geos215S1(:,2), 'r.');
+%%
+%Era Sorting 
+uniqueEras = unique(strata.Series_System);
+sortedData = sortrows(strata, 10);
+cambrianTable = sortedData(1:11,:);
+devonianTable = sortedData(12:45,:);
+lowerDevonianTable = sortedData(46:63,:);
+lowerMissTable = sortedData(64:71,:);
+lowerOrdTable = sortedData(72:77,:);
+midDevTable = sortedData(78:348,:);
+midOrdTable = sortedData(349:352,:);
+midUpDev = sortedData(353:362,:);
+missTable = sortedData(363:369,:);
+ordTable = sortedData(370:422,:);
+silTable = sortedData(423:461,:);
+
+upperCamTable = sortedData(462,:);
+
+upperDevTable = sortedData(463:884,:);
+
+upperOrdTable = sortedData(885:1043,:);
